72a246e9bfee96ab36048173ba5742c35eb646f8
NghiaCaNgao <nghiacangao@>
2023-06-03 15:19:35 +0700

    update vercel json

--- src/index.ts
+++ src/index.ts
@@ -32,6 +32,10 @@ app.post('/api/canxuongtinhso', async function (req, res) {
     res.json({ comment, poem })
 })
 
+app.get("/", (req, res)=>{
+    res.send("Hello World")
+})
+
 const server = app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
